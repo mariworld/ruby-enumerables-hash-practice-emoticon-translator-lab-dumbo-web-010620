3cd016fece6ad6f8e3b615888a33612415232ee3
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-31 03:56:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ end
 
 def get_japanese_emoticon(file_path = YAML.load_file('./lib/emoticons.yml'),emoticon)
   emo_list = load_library(file_path)
-  emo_list.each do |key,value| 
+  emo_list.each do |memo, (key,value)| 
       binding.pry
     end
 
