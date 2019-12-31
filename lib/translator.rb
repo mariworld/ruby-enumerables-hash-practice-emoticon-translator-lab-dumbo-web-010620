0707313184915fd5194da1b24d13a662f177bc38
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-31 03:54:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,8 @@ end
 def get_japanese_emoticon(file_path = YAML.load_file('./lib/emoticons.yml'),emoticon)
   emo_list = load_library(file_path)
   if emo_list[:get_meaning]
-    if emo_list[:get_emoticon]
+    if emo_list[:get_emoticon] == emo_list[:get_meaning]
+      
     
   end
   binding.pry
