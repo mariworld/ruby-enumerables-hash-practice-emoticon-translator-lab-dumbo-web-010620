56ebe02b82f839eda3ab70a4da21272456e122a7
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-31 03:43:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,25 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+    file_path = YAML.load_file('./lib/emoticons.yml')
+    new_hash = {
+      :get_meaning => {},
+      :get_emoticon => {}
+    }
+    translate_hash = file_path.reduce({}) do |memo, key|
+      new_hash[:get_meaning]["#{key[1][1]}"] = key[0]
+      new_hash[:get_emoticon]["#{key[1][0]}"] = key[1][1]
+      #new_hash[:get_meaning] = key[1][1]
+      #binding.pry
+      end
+    new_hash
 end
+  
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path = YAML.load_file('./lib/emoticons.yml'),emoticon)
+  emo_list = load_library(file_path)
 end
 
 def get_english_meaning
