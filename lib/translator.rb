70d636b9697676476ee2e1b3d9b8b9e873203ed9
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-31 04:18:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,7 @@ def get_japanese_emoticon(file_path = YAML.load_file('./lib/emoticons.yml'),emot
   j_emo = emo_list[:get_emoticon][emoticon]
   if j_emo
     j_emo
+  else
     puts "Sorry, that emoticon was not found"
   end
   #binding.pry
