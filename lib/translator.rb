be34ff5921124a79d74d89ad88a1ed19078d8a9f
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-31 04:17:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,8 @@ def get_japanese_emoticon(file_path = YAML.load_file('./lib/emoticons.yml'),emot
   emo_list = load_library(file_path)
   #we want to access emo_list[:get_emoticon] values when the grinning emoticon is typed
   j_emo = emo_list[:get_emoticon][emoticon]
-  if emoticon == nil
+  if j_emo
+    j_emo
     puts "Sorry, that emoticon was not found"
   end
   #binding.pry
